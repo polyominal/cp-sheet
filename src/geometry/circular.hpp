ee794cabe6494f832168d22a235bedee285691f3
polyominal <79344013+polyominal@users.noreply.github.com>
2024-02-11 13:56:44 +0800

    [circular] Update hashing layout

--- src/geometry/circular.hpp
+++ src/geometry/circular.hpp
@@ -27,16 +27,16 @@ struct C {
 
 // NOT TESTED
 // 0: outside; 1: on; 2: inside
-inline int contains(const C& c, const P& p) { /// start-hash
+inline int contains(const C& c, const P& p) {
 	return sgn(c.r - dist(p - c.c)) + 1;
-} /// end-hash
+}
 
 // 0-apart; 1-coincide;
 // 2-a<b; 3-a<=b;
 // 4-a>b; 5-a>=b;
 // 6-a touches b; 7-a cross b
-inline int insCC(const C& a, const C& b){ /// start-hash
-	D c = dist(a.c - b.c);
+inline int insCC(const C& a, const C& b) {
+	D c = dist(a.c - b.c); /// start-hash
 	if (sgn(c) == 0 && sgn(a.r, b.r) == 0) return 1;
 	int d = sgn(c + a.r - b.r);
 	if (d <= 0) return d+3;
@@ -44,22 +44,22 @@ inline int insCC(const C& a, const C& b){ /// start-hash
 	if (e <= 0) return e+5;
 	int f = sgn(c - a.r - b.r);
 	if (f <= 0) return -f+6;
-	return 0;
-} /// end-hash
+	return 0; /// end-hash
+}
 
-inline C incircle(const P& a, const P& b, const P& c) { /// start-hash
-	D da = dist(b - c);
+inline C incircle(const P& a, const P& b, const P& c) {
+	D da = dist(b - c); /// start-hash
 	D db = dist(a - c);
 	D dc = dist(a - b);
 	D s = da + db + dc;
 	return C(
 		(a * da + b * db + c * dc) / s,
 		std::abs(crs(b-a, c-a)) / s
-	);
-} /// end-hash
+	); /// end-hash
+}
 
-inline C outcircle(const P& a, P b, P c) { /// start-hash
-	b -= a, c -= a;
+inline C outcircle(const P& a, P b, P c) {
+	b -= a, c -= a; /// start-hash
 	D bb = dist2(b) / 2;
 	D cc = dist2(c) / 2;
 	D g = crs(b, c);
@@ -67,45 +67,45 @@ inline C outcircle(const P& a, P b, P c) { /// start-hash
 	D y = (b.x * cc - bb * c.x) / g;
 	D r = sqrt(x * x + y * y);
 	x += a.x, y += a.y;
-	return C(P(x, y), r);
-} /// end-hash
+	return C(P(x, y), r); /// end-hash
+}
 
-inline int crossCL(const C& c, const L& l, array<P, 2>& res) { /// start-hash
-	D u = distLP(l, c.c);
+inline int crossCL(const C& c, const L& l, array<P, 2>& res) {
+	D u = distLP(l, c.c); /// start-hash
 	int t = sgn(u, c.r);
 	if (t == 1) return 0;
 	P v = project(l, c.c);
 	P d = (t == 0 ? P(0, 0) : vec(l) * (sqrt(c.r * c.r - u * u) / dist(l)));
 	res = {v - d, v + d};
-	return 1 - t;
-} /// end-hash
+	return 1 - t; /// end-hash
+}
 
 // args of two intersections r, l seen by a.c,
 // assuming two circles cross
-inline pair<D, D> crossCC_args(const C& a, const C& b) { /// start-hash
-	P diff = b.c - a.c;
+inline pair<D, D> crossCC_args(const C& a, const C& b) {
+	P diff = b.c - a.c; /// start-hash
 	D c = arg(diff);
 	D d = arg(a.r, dist(diff), b.r);
-	return {c - d, c + d};
-} /// end-hash
+	return {c - d, c + d}; /// end-hash
+}
 
-inline int crossCC(const C& a, const C& b, array<P, 2>& res) { /// start-hash
-	int t = insCC(a, b);
+inline int crossCC(const C& a, const C& b, array<P, 2>& res) {
+	int t = insCC(a, b); /// start-hash
 	if (t == 0 || t == 1 || t == 2 || t == 4) return 0;
 	auto [l, r] = crossCC_args(a, b);
 	res = {eval(a, l), eval(a, r)};
-	return 2 - (t == 3 || t == 5 || t == 6);
-} /// end-hash
+	return 2 - (t == 3 || t == 5 || t == 6); /// end-hash
+}
 
-inline int tangent(const C& c, const P& p, array<P, 2>& res) { /// start-hash
-	P diff = p - c.c;
+inline int tangent(const C& c, const P& p, array<P, 2>& res) {
+	P diff = p - c.c; /// start-hash
 	D dd = dist(diff);
 	int t = sgn(c.r, dd);
 	if (t == 1) return 0;
 	D d = acos(min<D>(c.r / dd, 1));
 	D a = arg(diff);
 	res = {eval(c, a - d), eval(c, a + d)};
-	return 1 - t;
-} /// end-hash
+	return 1 - t; /// end-hash
+}
 
 } // namespace geometry
