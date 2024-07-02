540655468699b0cddf49da33da7cc79bfddab310
polyominal <79344013+polyominal@users.noreply.github.com>
2024-07-02 07:31:55 +0000

    Update YC

--- src/contest/base.hpp
+++ src/contest/base.hpp
@@ -11,16 +11,15 @@ template <class T> using Vec = vector<T>;
 
 using i64 = int64_t;
 
-template <class F> struct ycr { /// start-hash
+template <class F> struct yc_result { /// start-hash
 	F f;
-	template <class T> explicit ycr(T&& f_) : f(forward<T>(f_)) {}
-
+	template <class T> explicit yc_result(T&& f_) : f(std::forward<T>(f_)) {}
 	template <class... A> decltype(auto) operator()(A&&... as) {
-		return f(ref(*this), forward<A>(as)...);
+		return f(std::ref(*this), std::forward<A>(as)...);
 	}
 };
 template <class F> decltype(auto) yc(F&& f) {
-	return ycr<decay_t<F>>(forward<F>(f));
+	return yc_result<std::decay_t<F>>(std::forward<F>(f));
 } /// end-hash
 
 static mt19937_64 mt(chrono::steady_clock::now().time_since_epoch().count());
