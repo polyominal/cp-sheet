e06aa2176ae4ea5de52c5d6149f6cad797b96a5a
polyominal <79344013+polyominal@users.noreply.github.com>
2024-07-12 22:15:19 +0800

    [range-parallel-unionfind] Add range parallel unionfind

---
+++ src/data-structure/range-parallel-unionfind.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "data-structure/unionfind.hpp"
+
+template <class F> struct RangeParallelUnionFind {
+	Vec<UnionFind> uf;
+	const F f;
+	RangeParallelUnionFind(int n, F f_) : uf(bit_width<u32>(n), n), f(f_) {}
+	void inner(int a, int b, int k) {
+		if (auto result = uf[k].unite(a, b); result.has_value()) {
+			auto [p, v] = result.value();
+			if (k == 0) {
+				f(p, v);
+			} else {
+				k--;
+				inner(a, b, k);
+				inner(a + (1 << k), b + (1 << k), k);
+			}
+		}
+	}
+	void unite(int a, int b, int d) {
+		if (d == 0) return;
+		int k = bit_width<u32>(d) - 1;
+		inner(a, b, k);
+		inner(a + d - (1 << k), b + d - (1 << k), k);
+	}
+};
