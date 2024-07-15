fa7532f6a0539d1b8cea14a241ea63472ea7e9c6
polyominal <79344013+polyominal@users.noreply.github.com>
2024-07-15 13:25:04 +0000

    [tree-dp] Add unit test

--- src/ad-hoc/tree-dp.hpp
+++ src/ad-hoc/tree-dp.hpp
@@ -13,46 +13,47 @@
 
 #include "contest/base.hpp"
 
-template <class D, class E> struct TreeDP {
-	using S = D::S;
-	const Vec<Vec<E>>& g;
+// REQUIRED: n >= 1 and g represents (bidirectionally) a tree
+// int(E): edge -> index of its destination
+// TODO:
+// - subtree(v, p): subtree rooted at v with parent p
+// - Avoid recursion?
+template <class M, class E> struct TreeDP {
+	using S = M::S;
 	int n;
-	Vec<S> dp, dp2, res;
+	Vec<S> sub, psub, res;
 	Vec<E> par;
-
-	TreeDP(D d, const Vec<Vec<E>>& g_) : g(g_), n(int(g.size())), dp(n), dp2(n), res(n), par(n) {
-		assert(n >= 1);
+	TreeDP(M d, const Vec<Vec<E>>& g)
+		: n(int(size(g))), sub(n), psub(n), res(n), par(n) {
 		Vec<S> up(n), pref(n);
-
-		yc([&](auto self, int v, int p) -> void { /// start-hash
+		auto dfs_up = [&](auto self, int v, int p) -> void { /// start-hash
 			up[v] = d.make(v);
-			for (auto& e : g[v]) {
+			for (const auto& e : g[v]) {
 				if (e != p) {
-					self(e, v);
+					self(self, e, v);
 					pref[e] = up[v];
 					up[v] = d.op(up[v], up[e]);
 				} else {
 					par[v] = e;
 				}
 			}
-			dp[v] = up[v];
+			sub[v] = up[v];
 			if (p != -1) {
 				up[v] = d.up(up[v], par[v]);
 			}
-		})(0, -1); /// end-hash
-
-		yc([&](auto self, int v, int p, S f) -> void { /// start-hash
-			for (int j = int(g[v].size())-1; j >= 0; j--) {
-				auto& e = g[v][j];
+		};
+		dfs_up(dfs_up, 0, -1); /// end-hash
+		auto dfs_down = [&](auto self, int v, int p, S f) -> void { /// start-hash
+			for (const auto& e : g[v] | std::views::reverse) {
 				if (e == p) continue;
-				dp2[e] = d.op(f, pref[e]);
-				self(e, v, d.up(dp2[e], e));
+				psub[e] = d.op(f, pref[e]);
+				self(self, e, v, d.up(psub[e], e));
 				f = d.op(f, up[e]);
 			}
 			res[v] = f;
-		})(0, -1, d.make(0)); /// end-hash
+		};
+		dfs_down(dfs_down, 0, -1, d.make(0)); /// end-hash
 	}
-
 	const S& operator[](int i) const {
 		return res[i];
 	}
