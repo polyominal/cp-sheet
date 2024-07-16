3c37120a91d4e4dec761d6aca69372d32826a4e6
polyominal <79344013+polyominal@users.noreply.github.com>
2024-07-16 13:35:18 +0000

    Use actual tabs

--- src/oj-test/tree-path-composite-sum.test.cpp
+++ src/oj-test/tree-path-composite-sum.test.cpp
@@ -7,57 +7,57 @@
 using std::cin, std::cout;
 
 int main() {
-    int N;
-    cin >> N;
+	int N;
+	cin >> N;
 
-    using Z = ModInt<998244353>;
-    auto A = Vec<Z>(N);
-    for (auto& a : A) cin >> a.v;
+	using Z = ModInt<998244353>;
+	auto A = Vec<Z>(N);
+	for (auto& a : A) cin >> a.v;
 
-    struct E {
-        int dest;
-        int idx;
+	struct E {
+		int dest;
+		int idx;
 
-        operator int() const {
-            return dest;
-        }
-    };
-    auto adj = Vec<Vec<E>>(N);
-    auto B = Vec<Z>(N-1);
-    auto C = Vec<Z>(N-1);
-    for (int e = 0; e < N-1; e++) {
-        int u, v;
-        cin >> u >> v >> B[e].v >> C[e].v;
-        adj[u].push_back(E{v, e});
-        adj[v].push_back(E{u, e});
-    }
+		operator int() const {
+			return dest;
+		}
+	};
+	auto adj = Vec<Vec<E>>(N);
+	auto B = Vec<Z>(N-1);
+	auto C = Vec<Z>(N-1);
+	for (int e = 0; e < N-1; e++) {
+		int u, v;
+		cin >> u >> v >> B[e].v >> C[e].v;
+		adj[u].push_back(E{v, e});
+		adj[v].push_back(E{u, e});
+	}
 
-    struct Manager {
-        const Vec<Z>& A, B, C;
+	struct Manager {
+		const Vec<Z>& A, B, C;
 
-        struct Result {
-            Z top;
-            Z s;
-            Z c;
-        };
-        using S = Result;
+		struct Result {
+			Z top;
+			Z s;
+			Z c;
+		};
+		using S = Result;
 
-        S make(int v) {
-            return S{A[v], A[v], 1};
-        }
-        S op(const S& a, const S& b) {
-            assert(a.top == b.top);
-            return S{a.top, a.s + b.s - a.top, a.c + b.c - 1};
-        }
-        S up(const S& a, const E& e) {
-            return S{A[e.dest], A[e.dest] + a.s * B[e.idx] + a.c * C[e.idx], a.c + 1};
-        }
-    };
+		S make(int v) {
+			return S{A[v], A[v], 1};
+		}
+		S op(const S& a, const S& b) {
+			assert(a.top == b.top);
+			return S{a.top, a.s + b.s - a.top, a.c + b.c - 1};
+		}
+		S up(const S& a, const E& e) {
+			return S{A[e.dest], A[e.dest] + a.s * B[e.idx] + a.c * C[e.idx], a.c + 1};
+		}
+	};
 
-    auto tdp = TreeDP(Manager{.A=A, .B=B, .C=C}, adj);
-    for (int i = 0; i < N; i++) {
-        cout << tdp[i].s.v << '\n';
-    }
+	auto tdp = TreeDP(Manager{.A=A, .B=B, .C=C}, adj);
+	for (int i = 0; i < N; i++) {
+		cout << tdp[i].s.v << '\n';
+	}
 
-    return 0;
+	return 0;
 }
