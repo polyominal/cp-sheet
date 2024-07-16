673cbc08762a5a9a5e7abd8275c490b58b625af7
polyominal <79344013+polyominal@users.noreply.github.com>
2024-07-16 11:30:41 +0000

    More feasible container setup?

--- .devcontainer/extra.sh
+++ .devcontainer/extra.sh
@@ -2,10 +2,13 @@
 
 set -e
 
-sudo apt-get update
-sudo apt-get install -y cmake
+sudo cp ./.devcontainer/sources.list /etc/apt/sources.list
 
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple online-judge-tools online-judge-verify-helper
+sudo apt-get update
+
+sudo apt-get install -y clangd cmake python3 python3-pip
+
+pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple online-judge-tools online-judge-verify-helper
 
 # Reference: https://www.kenmuse.com/blog/avoiding-dubious-ownership-in-dev-containers/
 git config --global --add safe.directory $(pwd)
