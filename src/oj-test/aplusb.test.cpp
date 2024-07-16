333162f835007a7ed0fe547e9de7be0cdc7b6668
polyominal <79344013+polyominal@users.noreply.github.com>
2024-07-16 04:36:26 +0000

    Attempt to integrate oj-verify

---
+++ src/oj-test/aplusb.test.cpp
@@ -0,0 +1,12 @@
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/aplusb
+
+#include "contest/base.hpp"
+
+int main() {
+    int a, b;
+    using std::cin, std::cout;
+    cin >> a >> b;
+    cout << a+b << '\n';
+
+    return 0;
+}
