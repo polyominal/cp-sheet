890c1e4dab19db359020f8c26ac9809ef5767949
polyominal <79344013+polyominal@users.noreply.github.com>
2024-07-18 08:10:23 +0000

    [oj-test] point_set_range_composite

---
+++ oj-test/point-set-range-composite.test.cpp
@@ -0,0 +1,51 @@
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/point_set_range_composite
+
+#include <contest/base.hpp>
+#include <data-structure/segtree.hpp>
+#include <algebra/modint.hpp>
+#include <contest/fast-input.hpp>
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+
+	auto sc = fast_input::Scanner(stdin);
+
+	int N, Q;
+	sc.read(N, Q);
+
+	using Z = ModInt<998244353>;
+	struct Monoid {
+		using S = pair<Z, Z>;
+
+		S e() const { return S{1, 0}; }
+		S op(const S& a, const S& b) const {
+			return S{a.first * b.first, a.second * b.first + b.second};
+		}
+	};
+
+	auto segtree = Segtree(N, [&](int) -> Monoid::S {
+		u32 a, b;
+		sc.read(a, b);
+		return Monoid::S{a, b};
+	}, Monoid());
+
+	for (int q = 0; q < Q; q++) {
+		int t;
+		sc.read(t);
+		if (t == 0) {
+			int p;
+			u32 c, d;
+			sc.read(p, c, d);
+			segtree.set(p, Monoid::S{c, d});
+		} else if (t == 1) {
+			int l, r;
+			Z x;
+			sc.read(l, r, x.v);
+			auto f = segtree.prod(l, r);
+			Z res = f.first * x + f.second;
+			std::cout << res.v << '\n';
+		} else assert(false);
+	}
+
+	return 0;
+}
