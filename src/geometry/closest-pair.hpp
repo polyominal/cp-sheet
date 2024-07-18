1156beb65c922f258a0f8801847c3d99d5576000
polyominal <79344013+polyominal@users.noreply.github.com>
2024-07-18 11:29:27 +0000

    [closest-pair] Use an alternative implementation

--- src/geometry/closest-pair.hpp
+++ src/geometry/closest-pair.hpp
@@ -1,11 +1,9 @@
 #pragma once
 
 /**
- * Description: Given a set of points, returns the
- * squared distance between the closest pair(s)
- * Source: https://github.com/anroysko/lETHargy-ctl/blob/main/content/geometry/closest-pair.cpp
- * Status: Tested with https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/all/CGL_5_A
- * Time: O(N \log^2 N) but practically fast
+ * Description: Given a set of points, returns an arbitrary closest pair of points.
+ * Source: https://judge.yosupo.jp/submission/214022
+ * Status: Tested with https://judge.yosupo.jp/problem/closest_pair
  */
 
 #include "contest/base.hpp"
@@ -14,41 +12,59 @@ namespace closest_pair_impl {
 
 template <class T> using P = pair<T, T>;
 
+// PRECONDITION: There are at least 2 points
 template <class T> inline tuple<T, P<T>, P<T>> closest_pair(Vec<P<T>> pts) {
-	assert(size(pts) >= 2);
-	std::ranges::sort(pts, [](const auto& a, const auto& b) -> bool {
-		return a.first < b.first;
-	});
-	using R = tuple<T, P<T>, P<T>>;
-	auto sq = [&](T a) -> T {
-		return a*a;
+	int n = int(size(pts));
+	using PT = P<T>;
+	std::ranges::sort(pts,
+					  [](PT a, PT b) -> bool { return a.first < b.first; });
+	auto sq = [&](T a) -> T { return a * a; };
+	auto dist2 = [&](PT a, PT b) -> T {
+		return sq(a.first - b.first) + sq(a.second - b.second);
 	};
-	auto dist2 = [&](const auto& a, const auto& b) -> T {
-		return sq(a.first-b.first) + sq(a.second-b.second);
-	};
-	auto best = R(dist2(pts[0], pts[1]), pts[0], pts[1]);
-	auto rec = [&](auto self, int l, int r) -> void {
-		if (l+1 == r) return;
-		int m = (l+r)/2;
-		self(self, l, m), self(self, m, r);
 
-		auto cnds = Vec<P<T>>();
-		for (int i = l; i < r; i++) {
-			T dx = (pts[i].first - pts[m-1].first);
-			if (dx * dx <= get<0>(best)) cnds.push_back(pts[i]);
+	T d = std::numeric_limits<T>::max();
+	PT pa, pb;
+	auto update = [&](PT a, PT b) {
+		auto nd = dist2(a, b);
+		if (nd < d) {
+			d = nd, pa = a, pb = b;
 		}
-		std::ranges::sort(cnds, [](const auto& a, const auto& b) -> bool {
-			return a.second < b.second;
-		});
-		int nc = int(size(cnds));
-		for (int i = 0; i < nc; i++) {
-			for (int j = i+1; j < i+7 && j < nc; j++) {
-				best = min(best, R(dist2(cnds[i], cnds[j]), cnds[i], cnds[j]));
+	};
+
+	auto st =
+		multiset<PT,
+				 decltype([](PT a, PT b) { return a.second < b.second; })>();
+	auto its = Vec<typename decltype(st)::const_iterator>(size(pts));
+
+	for (int i = 0, f = 0; i < n; i++) {
+		PT p = pts[i];
+		while (f < i && sq(p.first - pts[f].first) >= d) {
+			st.erase(its[f++]);
+		}
+		auto u = st.upper_bound(p);
+		{
+			auto t = u;
+			while (true) {
+				if (t == begin(st)) break;
+				t = prev(t);
+				update(*t, p);
+				if (sq(p.second - t->second) >= d) break;
 			}
 		}
-	};
-	rec(rec, 0, int(size(pts)));
-	return best;
+		{
+			auto t = u;
+			while (true) {
+				if (t == end(st)) break;
+				if (sq(p.second - t->second) >= d) break;
+				update(*t, p);
+				t = next(t);
+			}
+		}
+		its[i] = st.emplace_hint(u, p);
+	}
+
+	return {d, pa, pb};
 }
 
-} // namespace closest_pair_impl
+}  // namespace closest_pair_impl
--- src/geometry/closest-pair_test.cpp
+++ src/geometry/closest-pair_test.cpp
@@ -1,17 +1,13 @@
+#include <gtest/gtest.h>
 #include <contest/base.hpp>
 #include <geometry/closest-pair.hpp>
-#include <gtest/gtest.h>
 
 TEST(ClosestPairTest, Sample) {
 	using P = pair<i64, i64>;
 
 	{
 		auto pts = Vec<P>{
-			P(-1, -1),
-			P(-6, 4),
-			P(-9, -7),
-			P(2, 5),
-			P(-7, 6),
+			P(-1, -1), P(-6, 4), P(-9, -7), P(2, 5), P(-7, 6),
 		};
 
 		// P[1] and P[4]: 0^2 + 2^2 = 5
