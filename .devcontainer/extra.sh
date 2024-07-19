115850527641baba6d35e561eaffe9675865ac31
polyominal <79344013+polyominal@users.noreply.github.com>
2024-07-19 21:02:51 +0800

    Update container

--- .devcontainer/extra.sh
+++ .devcontainer/extra.sh
@@ -39,17 +39,10 @@ github_download() {
 
 echo "Installing essential tools..."
 
-# Choose between amd64 mirror or arm64 mirror
-if [ "${ARCHITECTURE}" = "amd64" ]; then
-    sudo cp ./.devcontainer/sources.list.amd64 /etc/apt/sources.list
-else
-    sudo cp ./.devcontainer/sources.list.arm64 /etc/apt/sources.list
-fi
-
 sudo apt-get update
 
 sudo apt-get install -y clangd cmake python3-pip
-pip install -i https://pypi.tuna.tsinghua.edu.cn/simple online-judge-tools online-judge-verify-helper
+pip install online-judge-tools online-judge-verify-helper
 
 echo "Configuring Git..."
 
