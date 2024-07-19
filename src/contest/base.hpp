e2f822e2556ba02b7a9e46ee067d2e0bfa10ba17
polyominal <79344013+polyominal@users.noreply.github.com>
2024-07-19 17:58:18 +0000

    [base] Update and format

--- src/contest/base.hpp
+++ src/contest/base.hpp
@@ -2,12 +2,13 @@
 
 #include <bits/stdc++.h>
 
-using std::vector, std::array, std::string;
-using std::set, std::map, std::multiset;
+using std::abs, std::sin, std::cos, std::tan, std::asin, std::acos, std::atan2;
 using std::min, std::max, std::swap;
 using std::pair, std::tuple;
+using std::set, std::map, std::multiset;
+using std::sqrt, std::cbrt;
 using std::tie;
-using std::abs, std::sin, std::cos, std::tan, std::asin, std::acos, std::atan2;
+using std::vector, std::array, std::string;
 
 template <class T> using Vec = vector<T>;
 template <class T> using Opt = std::optional<T>;
@@ -18,7 +19,7 @@ using u32 = uint32_t;
 using u64 = uint64_t;
 using u128 = __uint128_t;
 
-template <class F> struct yc_result { /// start-hash
+template <class F> struct yc_result {  /// start-hash
 	F f;
 	template <class T> explicit yc_result(T&& f_) : f(std::forward<T>(f_)) {}
 	template <class... A> decltype(auto) operator()(A&&... as) {
@@ -27,6 +28,7 @@ template <class F> struct yc_result { /// start-hash
 };
 template <class F> decltype(auto) yc(F&& f) {
 	return yc_result<std::decay_t<F>>(std::forward<F>(f));
-} /// end-hash
+}  /// end-hash
 
-inline std::mt19937_64 mt(std::chrono::steady_clock::now().time_since_epoch().count());
+inline std::mt19937_64 mt(
+	std::chrono::steady_clock::now().time_since_epoch().count());
