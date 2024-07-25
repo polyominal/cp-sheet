b2bba17e5428d27844c76b9835beea2f4a6f5f7e
polyominal <79344013+polyominal@users.noreply.github.com>
2024-07-25 21:16:57 +0800

    [floor-ceil-range] Fix and test

---
+++ oj-test/number-theory/enumerate_quotients.test.cpp
@@ -0,0 +1,23 @@
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/enumerate_quotients
+
+#include "ad-hoc/floor-ceil-range.hpp"
+#include "contest/base.hpp"
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	i64 N;
+	std::cin >> N;
+
+	auto quotients = Vec<i64>();
+	floor_range(N, [&](i64 q, i64, i64) -> void { quotients.push_back(q); });
+
+	ssize_t num = ssize(quotients);
+	std::cout << num << '\n';
+	for (ssize_t i = 0; i < num; i++) {
+		std::cout << quotients[i] << " \n"[i + 1 == num];
+	}
+
+	return 0;
+}
--- src/ad-hoc/floor-ceil-range.hpp
+++ src/ad-hoc/floor-ceil-range.hpp
@@ -1,16 +1,18 @@
+#pragma once
+
 /**
  * Author: Hanfei Chen
  * Date: 2024-01-12
- * Status: Unit tested
+ * Status:
+ * - Unit tested
+ * - floor_range tested with https://judge.yosupo.jp/problem/enumerate_quotients
  */
-
-#pragma once
 
 #include "contest/base.hpp"
 
 inline void floor_range(i64 n, auto f) {
-	int rt = int(sqrtl(n)); /// start-hash
-	int num = (rt * rt + rt <= n ? rt : rt - 1);
+	int rt = int(sqrt(double(n)));	/// start-hash
+	int num = (i64(rt) * rt + rt <= n ? rt : rt - 1);
 	i64 prv = n + 1;
 	for (int q = 1; q <= num; q++) {
 		i64 x = i64(double(n) / (q + 1)) + 1;
@@ -18,21 +20,21 @@ inline void floor_range(i64 n, auto f) {
 		prv = x;
 	}
 	for (int l = rt; l >= 1; l--) {
-		f(i64(double(n) / l), l, l+1);
-	} /// end-hash
+		f(i64(double(n) / l), l, l + 1);
+	}  /// end-hash
 }
 
 inline void ceil_range(i64 n, auto f) {
-  	int rt = int(sqrtl(n)); /// start-hash
-  	i64 prv = std::numeric_limits<i64>::max();
-  	for (int q = 1; q <= rt; ++q) {
+	int rt = int(sqrt(double(n)));	/// start-hash
+	i64 prv = std::numeric_limits<i64>::max();
+	for (int q = 1; q <= rt; ++q) {
 		i64 x = i64(double(n + q - 1) / q);
 		f(q, x, prv);
 		prv = x;
-  	}
-  	int num = (n <= rt * rt + rt ? rt : rt + 1);
-  	if (n == rt * rt) --num;
-  	for (int l = num; l >= 1; --l) {
-		f(i64(double(n + l - 1) / l), l, l+1);
-	} /// end-hash
+	}
+	int num = (n <= i64(rt) * rt + rt ? rt : rt + 1);
+	if (n == rt * rt) --num;
+	for (int l = num; l >= 1; --l) {
+		f(i64(double(n + l - 1) / l), l, l + 1);
+	}  /// end-hash
 }
