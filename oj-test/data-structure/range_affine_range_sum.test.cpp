e9c711113663838a42689a8d5735630d76976d05
polyominal <79344013+polyominal@users.noreply.github.com>
2024-08-05 07:59:08 +0000

    [lazy-segtree] Verify

---
+++ oj-test/data-structure/range_affine_range_sum.test.cpp
@@ -0,0 +1,62 @@
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/range_affine_range_sum
+
+#include "algebra/modint.hpp"
+#include "contest/base.hpp"
+#include "contest/fast-input.hpp"
+#include "data-structure/lazy-segtree.hpp"
+
+int main() {
+	using fast_input::Scanner;
+	auto sc = Scanner(stdin);
+
+	std::ios_base::sync_with_stdio(false);
+
+	int N, Q;
+	sc.read(N, Q);
+
+	constexpr u32 MOD = 998244353;
+	using Z = ModInt<MOD>;
+
+	struct RangeAffineMonoid {
+		using S = pair<Z, int>;
+		using F = pair<Z, Z>;
+
+		constexpr S e() { return {0, 0}; }
+		S op(S a, S b) { return {a.first + b.first, a.second + b.second}; }
+		S mapping(F f, S a) {
+			return {a.first * f.first + a.second * f.second, a.second};
+		}
+
+		constexpr F id() { return {1, 0}; }
+		F composition(F f, F g) {
+			return {f.first * g.first, f.second * g.first + g.second};
+		}
+	} m;
+
+	auto st = LazySegtree(
+		N,
+		[&](int) -> RangeAffineMonoid::S {
+			Z a;
+			sc.read(a.v);
+			return {a, 1};
+		},
+		m);
+
+	for (int q = 0; q < Q; q++) {
+		int t, l, r;
+		sc.read(t, l, r);
+		if (t == 0) {
+			Z b, c;
+			sc.read(b.v, c.v);
+			st.apply(l, r, {b, c});
+		} else if (t == 1) {
+			Z tot = 0;
+			st.enumerate(l, r, [&](const RangeAffineMonoid::S& s) -> void {
+				tot += s.first;
+			});
+			std::cout << tot.v << '\n';
+		}
+	}
+
+	return 0;
+}
--- src/data-structure/lazy-segtree.hpp
+++ src/data-structure/lazy-segtree.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 /**
  * Author: Hanfei Chen
  * Description: Lazy segtree abstraction
@@ -10,8 +12,6 @@
  * - https://judge.yosupo.jp/problem/vertex_set_path_composite (set; also justified runtime polymorphism!)
  */
 
-#pragma once
-
 #include "contest/base.hpp"
 
 template <class M> struct LazySegtree {
@@ -22,56 +22,54 @@ template <class M> struct LazySegtree {
 	Vec<F> lz;
 	int n, h, sz;
 	LazySegtree(M m_) : m(m_), n(0), h(0), sz(0) {}
-	template <class A> LazySegtree(int n_, A a, M m_) : m(m_) {
-		build(n_, a);
-	}
-	template <class A> void build(int n_, A a) { /// start-hash
+	template <class A> LazySegtree(int n_, A a, M m_) : m(m_) { build(n_, a); }
+	template <class A> void build(int n_, A a) {  /// start-hash
 		n = n_;
 		sz = std::bit_ceil<uint32_t>(n);
 		h = std::countr_zero<uint32_t>(sz);
-		d.resize(2*sz);
+		d.resize(2 * sz);
 		lz.assign(sz, m.id());
-		for (int i = 0; i < n; i++) d[sz+i] = a(i);
-		for (int i = n; i < sz; i++) d[sz+i] = m.e();
-		for (int i = sz-1; i >= 1; i--) update(i);
-	} /// end-hash
+		for (int i = 0; i < n; i++) d[sz + i] = a(i);
+		for (int i = n; i < sz; i++) d[sz + i] = m.e();
+		for (int i = sz - 1; i >= 1; i--) update(i);
+	}  /// end-hash
 
-	void update(int i){ /// start-hash
-		d[i] = m.op(d[2*i], d[2*i+1]);
-	} /// end-hash
+	void update(int i) {  /// start-hash
+		d[i] = m.op(d[2 * i], d[2 * i + 1]);
+	}  /// end-hash
 
-	void apply(int i, F f) { /// start-hash
+	void apply(int i, F f) {  /// start-hash
 		d[i] = m.mapping(f, d[i]);
 		if (i < sz) lz[i] = m.composition(lz[i], f);
-	} /// end-hash
+	}  /// end-hash
 
-	void downdate(int i) { /// start-hash
-		apply(2*i, lz[i]);
-		apply(2*i+1, lz[i]);
+	void downdate(int i) {	/// start-hash
+		apply(2 * i, lz[i]);
+		apply(2 * i + 1, lz[i]);
 		lz[i] = m.id();
-	} /// end-hash
+	}  /// end-hash
 
-	void downdate_range(int l, int r) { /// start-hash
+	void downdate_range(int l, int r) {	 /// start-hash
 		l += sz, r += sz;
 		for (int i = h; i >= 1; i--) {
 			if (((l >> i) << i) != l) downdate(l >> i);
-			if (((r >> i) << i) != r) downdate((r-1) >> i);
+			if (((r >> i) << i) != r) downdate((r - 1) >> i);
 		}
-	} /// end-hash
+	}  /// end-hash
 
-	S prod(int l, int r) { /// start-hash
+	S prod(int l, int r) {	/// start-hash
 		assert(0 <= l && l <= r && r <= n);
 		if (l == r) return m.e();
 		downdate_range(l, r);
 		S sl = m.e(), sr = m.e();
-		for (int a = l+sz, b = r+sz; a < b; a /= 2, b /= 2) {
+		for (int a = l + sz, b = r + sz; a < b; a /= 2, b /= 2) {
 			if (a & 1) sl = m.op(sl, d[a++]);
 			if (b & 1) sr = m.op(d[--b], sr);
 		}
 		return m.op(sl, sr);
-	} /// end-hash
+	}  /// end-hash
 
-	void apply(int l, int r, F f) { /// start-hash
+	void apply(int l, int r, F f) {	 /// start-hash
 		assert(0 <= l && l <= r && r <= n);
 		if (l == r) return;
 		downdate_range(l, r);
@@ -82,46 +80,46 @@ template <class M> struct LazySegtree {
 		}
 		for (int i = 1; i <= h; i++) {
 			if (((l >> i) << i) != l) update(l >> i);
-			if (((r >> i) << i) != r) update((r-1) >> i);
+			if (((r >> i) << i) != r) update((r - 1) >> i);
 		}
-	} /// end-hash
+	}  /// end-hash
 
 	// You can use this to query stuff,
 	// which is sometimes more efficient than using prod
-	template <class G> void enumerate(int l, int r, G g) { /// start-hash
+	template <class G> void enumerate(int l, int r, G g) {	/// start-hash
 		assert(0 <= l && l <= r && r <= n);
 		if (l == r) return;
 		downdate_range(l, r);
-		for (int a = l+sz, b = r+sz; a < b; a /= 2, b /= 2) {
+		for (int a = l + sz, b = r + sz; a < b; a /= 2, b /= 2) {
 			if (a & 1) g(d[a++]);
 			if (b & 1) g(d[--b]);
 		}
-	} /// end-hash
+	}  /// end-hash
 
 	// Enumerating in some sequential order
 	template <bool l_to_r = true, class G>
 	void enumerate_in_order(int l, int r, G g) {
 		assert(0 <= l && l <= r && r <= n);
-		if (l == r) return; /// start-hash
+		if (l == r) return;	 /// start-hash
 		downdate_range(l, r);
 		static Vec<int> ls, rs;
 		ls.clear(), rs.clear();
-		for (int a = l+sz, b = r+sz; a < b; a /= 2, b /= 2) {
+		for (int a = l + sz, b = r + sz; a < b; a /= 2, b /= 2) {
 			if (a & 1) ls.push_back(a++);
 			if (b & 1) rs.push_back(--b);
-		} /// end-hash
+		}  /// end-hash
 		if constexpr (l_to_r) {
 			for (int i : ls) g(d[i]);
-			for (int z = int(rs.size())-1; z >= 0; z--) g(d[rs[z]]);
+			for (int z = int(rs.size()) - 1; z >= 0; z--) g(d[rs[z]]);
 		} else {
 			for (int i : rs) g(d[i]);
-			for (int z = int(ls.size())-1; z >= 0; z--) g(d[ls[z]]);
+			for (int z = int(ls.size()) - 1; z >= 0; z--) g(d[ls[z]]);
 		}
 	}
 
 	const S& all_prod() const { return d[1]; }
 
-	template <class P> pair<int, S> max_right(int l, P p) { /// start-hash
+	template <class P> pair<int, S> max_right(int l, P p) {	 /// start-hash
 		assert(0 <= l && l <= n);
 		if (l == n) return {n, m.e()};
 		l += sz;
@@ -140,19 +138,19 @@ template <class M> struct LazySegtree {
 						l++;
 					}
 				}
-				return {l-sz, s};
+				return {l - sz, s};
 			}
 			s = m.op(s, d[l]);
 			l++;
 		} while ((l & -l) != l);
 		return {n, s};
-	} /// end-hash
+	}  /// end-hash
 
-	template <class P> pair<int, S> min_left(int r, P p) { /// start-hash
+	template <class P> pair<int, S> min_left(int r, P p) {	/// start-hash
 		assert(0 <= r && r <= n);
 		if (r == 0) return {0, m.e()};
 		r += sz;
-		for (int i = h; i >= 1; i--) downdate((r-1) >> i);
+		for (int i = h; i >= 1; i--) downdate((r - 1) >> i);
 		S s = m.e();
 		assert(p(s));
 		do {
@@ -168,18 +166,18 @@ template <class M> struct LazySegtree {
 						r--;
 					}
 				}
-				return {r+1-sz, s};
+				return {r + 1 - sz, s};
 			}
 			s = m.op(d[r], s);
 		} while ((r & -r) != r);
 		return {0, s};
-	} /// end-hash
+	}  /// end-hash
 
-	void set(int p, S s) { /// start-hash
+	void set(int p, S s) {	/// start-hash
 		assert(0 <= p && p < n);
 		p += sz;
 		for (int i = h; i >= 1; i--) downdate(p >> i);
 		d[p] = s;
 		for (int i = 1; i <= h; i++) update(p >> i);
-	} /// end-hash
+	}  /// end-hash
 };
