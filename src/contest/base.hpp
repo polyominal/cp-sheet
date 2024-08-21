1c6e3db0903859e97db275eb11a69565125c374a
polyominal <79344013+polyominal@users.noreply.github.com>
2024-08-21 12:00:45 +0000

    [base] Update

--- src/contest/base.hpp
+++ src/contest/base.hpp
@@ -16,18 +16,8 @@ using i32 = int32_t;
 using i64 = int64_t;
 using u32 = uint32_t;
 using u64 = uint64_t;
+using i128 = __int128_t;
 using u128 = __uint128_t;
-
-template <class F> struct yc_result {  /// start-hash
-	F f;
-	template <class T> explicit yc_result(T&& f_) : f(std::forward<T>(f_)) {}
-	template <class... A> decltype(auto) operator()(A&&... as) {
-		return f(std::ref(*this), std::forward<A>(as)...);
-	}
-};
-template <class F> decltype(auto) yc(F&& f) {
-	return yc_result<std::decay_t<F>>(std::forward<F>(f));
-}  /// end-hash
 
 inline std::mt19937_64 mt(
 	std::chrono::steady_clock::now().time_since_epoch().count());
