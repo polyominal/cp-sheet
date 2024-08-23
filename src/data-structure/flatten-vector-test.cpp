44500222a2faf2afe21445ae50895066ca8c4e0a
polyominal <79344013+polyominal@users.noreply.github.com>
2024-08-23 18:05:44 +0800

    feat: implement flatten-vector

---
+++ src/data-structure/flatten-vector-test.cpp
@@ -0,0 +1,12 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <data-structure/flatten-vector.hpp>
+
+TEST(TestFlattenVector, Example) {
+	// Copied from
+	// <https://github.com/yosupo06/yosupo-library/blob/15046ee7dafa1ba60174fd37b5c03ee9cbe444bf/test/unittest/flattenvector_test.cpp>
+	auto fv = FlattenVector<int>(
+		3, Vec<pair<int, int>>{{0, 1}, {0, 2}, {1, 10}, {2, 20}, {1, 11}});
+	EXPECT_THAT(fv.off, testing::ElementsAre(0, 2, 4, 5));
+	EXPECT_THAT(fv.v, testing::ElementsAre(1, 2, 10, 11, 20));
+}
---
+++ src/data-structure/flatten-vector.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "contest/base.hpp"
+
+template <class T> struct FlattenVector {
+	Vec<T> v;
+	Vec<int> off;
+	template <class U> FlattenVector(int n, U&& a) : off(n + 1) {
+		for (const auto& [i, _] : a) {
+			off[i + 1]++;
+		}
+		std::partial_sum(begin(off), end(off), begin(off));
+
+		v = std::vector<T>(off[n]);
+		auto pt = off;
+		for (auto&& [i, x] : std::forward<U>(a)) {
+			v[pt[i]++] = std::move(x);
+		}
+	}
+
+	auto at(int i) const {
+		return v | std::views::take(off[i + 1]) | std::views::drop(off[i]);
+	}
+};
