5693b0663b9c53406892fbed79adc2a56b8d9a3b
polyominal <79344013+polyominal@users.noreply.github.com>
2024-09-13 20:11:27 +0800

    feat: dominator tree

---
+++ oj-test/lc/graph/dominatortree.test.cpp
@@ -0,0 +1,24 @@
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/dominatortree
+
+#include "data-structure/flatten-vector.hpp"
+#include "graph/dominator.hpp"
+
+int main() {
+	using namespace std;
+
+	int n, m, s;
+	cin >> n >> m >> s;
+	auto edges = Vec<pair<int, int>>(m);
+	for (auto& [a, b] : edges) {
+		cin >> a >> b;
+	}
+
+	auto g = FlattenVector<int>(n, edges);
+	auto idom = build_idom(n, g, s);
+	idom[s] = s;
+	for (int i = 0; i < n; i++) {
+		cout << idom[i] << " \n"[i + 1 == n];
+	}
+
+	return 0;
+}
