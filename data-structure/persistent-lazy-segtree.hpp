b6e42c8a6254e18158706e9810d6a091ad3d0cb8
polyominal <endotrauma97@gmail.com>
2025-03-15 15:36:15 +0500

    refactor: revise persistent lazy segtree

--- data-structure/persistent-lazy-segtree.hpp
+++ data-structure/persistent-lazy-segtree.hpp
@@ -8,12 +8,15 @@ template <typename T, typename E>
     requires Monoid<T> && Effect<E, T>
 class PersistentLazySegmentTree {
   public:
-    PersistentLazySegmentTree(size_t capacity = 0) { pool.reserve(capacity); }
+    explicit PersistentLazySegmentTree(size_t capacity = 0) {
+        if (capacity > 0) {
+            pool.reserve(capacity);
+        }
+    }
 
     size_t build(std::span<const T> data) {
         if (data.size() == 1) {
-            return create_node(std::move(T(data[0])), std::move(E::e()), NONE,
-                               NONE);
+            return create_node(T(data[0]), E::e(), NONE, NONE);
         }
         size_t m = data.size() / 2;
         size_t left = build(data.subspan(0, m));
@@ -21,11 +24,18 @@ class PersistentLazySegmentTree {
         return merge(left, right);
     }
 
-    size_t apply(size_t root, size_t l, size_t r, const E& f, size_t n) {
+    size_t apply(size_t root,
+                 size_t l,
+                 size_t r,
+                 const E& f,
+                 size_t n) noexcept {
         return apply_impl(root, 0, n, l, r, E::e(), f);
     }
 
-    T prod(size_t root, size_t l, size_t r, size_t n) const {
+    [[nodiscard]] T prod(size_t root,
+                         size_t l,
+                         size_t r,
+                         size_t n) const noexcept {
         return prod_impl(root, 0, n, l, r, E::e());
     }
 
@@ -33,7 +43,7 @@ class PersistentLazySegmentTree {
                 size_t mutator_root,
                 size_t l,
                 size_t r,
-                size_t n) {
+                size_t n) noexcept {
         return copy_impl(src_root, mutator_root, 0, n, l, r, E::e(), E::e());
     }
 
@@ -44,15 +54,17 @@ class PersistentLazySegmentTree {
         size_t left;
         size_t right;
 
-        Node(T&& val_, E&& lazy_, size_t l_, size_t r_)
+        Node(T val_, E lazy_, size_t l_, size_t r_) noexcept
             : val(std::move(val_)),
               lazy(std::move(lazy_)),
               left(l_),
               right(r_) {}
     };
 
-    size_t create_node(T&& val, E&& lazy, size_t left, size_t right) {
-        pool.emplace_back(std::move(val), std::move(lazy), left, right);
+    template <typename TVal, typename ELazy>
+    size_t create_node(TVal&& val, ELazy&& lazy, size_t left, size_t right) {
+        pool.emplace_back(std::forward<TVal>(val), std::forward<ELazy>(lazy),
+                          left, right);
         return pool.size() - 1;
     }
 
@@ -65,8 +77,10 @@ class PersistentLazySegmentTree {
     }
 
     size_t merge(size_t left, size_t right) {
-        return create_node(std::move(pool[left].val.merge(pool[right].val)),
-                           std::move(E::e()), left, right);
+        const auto& left_node = pool[left];
+        const auto& right_node = pool[right];
+        T merged_val = left_node.val.merge(right_node.val);
+        return create_node(std::move(merged_val), E::e(), left, right);
     }
 
     size_t apply_impl(size_t idx,
@@ -77,7 +91,8 @@ class PersistentLazySegmentTree {
                       const E& laz,
                       const E& f) {
         if (l <= s && e <= r) {
-            return apply_all(idx, laz.merge(f));
+            E combined = laz.merge(f);
+            return apply_all(idx, combined);
         }
         if (r <= s || e <= l) {
             return apply_all(idx, laz);
@@ -87,8 +102,11 @@ class PersistentLazySegmentTree {
         E new_laz = pool[idx].lazy.merge(laz);
         size_t left = pool[idx].left;
         size_t right = pool[idx].right;
-        return merge(apply_impl(left, s, m, l, r, new_laz, f),
-                     apply_impl(right, m, e, l, r, new_laz, f));
+
+        size_t new_left = apply_impl(left, s, m, l, r, new_laz, f);
+        size_t new_right = apply_impl(right, m, e, l, r, new_laz, f);
+
+        return merge(new_left, new_right);
     }
 
     T prod_impl(size_t idx,
@@ -111,8 +129,9 @@ class PersistentLazySegmentTree {
         } else if (m <= l) {
             return prod_impl(right, m, e, l, r, new_laz);
         } else {
-            return prod_impl(left, s, m, l, r, new_laz)
-                .merge(prod_impl(right, m, e, l, r, new_laz));
+            T left_result = prod_impl(left, s, m, l, r, new_laz);
+            T right_result = prod_impl(right, m, e, l, r, new_laz);
+            return left_result.merge(right_result);
         }
     }
 
@@ -139,10 +158,12 @@ class PersistentLazySegmentTree {
         size_t mutator_left = pool[mutator_idx].left;
         size_t mutator_right = pool[mutator_idx].right;
 
-        return merge(copy_impl(src_left, mutator_left, s, m, l, r, new_src_laz,
-                               new_mutator_laz),
-                     copy_impl(src_right, mutator_right, m, e, l, r,
-                               new_src_laz, new_mutator_laz));
+        size_t new_left = copy_impl(src_left, mutator_left, s, m, l, r,
+                                    new_src_laz, new_mutator_laz);
+        size_t new_right = copy_impl(src_right, mutator_right, m, e, l, r,
+                                     new_src_laz, new_mutator_laz);
+
+        return merge(new_left, new_right);
     }
 
     std::vector<Node> pool;
