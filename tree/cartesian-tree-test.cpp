872433c7d2cb1d2bab9cf2c566568693276d702c
Hanfei Chen <79344013+polyominal@users.noreply.github.com>
2025-04-02 22:24:15 +0800

    refactor: Cartesian tree update (#1)

--- tree/cartesian-tree-test.cpp
+++ tree/cartesian-tree-test.cpp
@@ -1,13 +1,14 @@
 #include "tree/cartesian-tree.hpp"
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include "contest/base.hpp"
 
 namespace testing {
 
 TEST(TestCartesianTree, Example) {
     auto A = Vec<int>{9, 3, 7, 1, 8, 12, 10, 20, 15, 18, 5};
     auto ct = CartesianTree(A);
-    EXPECT_THAT(ct.p, ElementsAre(1, 3, 1, -1, 10, 6, 4, 8, 6, 8, 3));
+    EXPECT_THAT(ct.par, ElementsAre(1, 3, 1, -1, 10, 6, 4, 8, 6, 8, 3));
 }
 
 }  // namespace testing
