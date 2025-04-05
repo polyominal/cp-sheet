5878fbd63843a7c8792977e54db3551ee380fc77
polyominal <79344013+polyominal@users.noreply.github.com>
2025-04-05 10:51:38 +0000

    credits

--- oj-test/run_oj.sh
+++ oj-test/run_oj.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Copied: https://github.com/yosupo06/yosupo-library/blob/5b768522ff3a8587ebe40b7bba5258f5ec18ad92/test/run_oj.sh
+
 expected_dir="$(cd "$(dirname "$0")/.." && pwd)"
 if [ "$(pwd)" != "$expected_dir" ]; then
   echo "Error: Please run the script from ${expected_dir}."
